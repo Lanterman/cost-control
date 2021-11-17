e7f6be4b0fb5ad9273d74c8f7fd3987a0e0162b6
Lanterman <splen1011@mail.ru>
2021-11-17 17:42:40 +0300

    3 Added new buttons, modernized Add, Delete

---
+++ database.py
@@ -0,0 +1,26 @@
+import sqlite3
+from datetime import datetime
+
+
+class DataBase:
+    def __init__(self):
+        self.connection = sqlite3.connect("control.db")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS control (
+            id integer primary key,
+            description text,
+            costs text,
+            price real,
+            date text)
+            """
+        )
+        self.connection.commit()
+
+    def insert_data(self, description, costs, price):
+        self.cursor.execute(
+            """INSERT INTO control (description, costs, price, date) VALUES (?, ?, ?, ?)""",
+            (description, costs, price, str(datetime.now())[:19])
+        )
+        self.connection.commit()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from datetime import datetime
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import sqlite3
+
+from database import DataBase
 
 
 class Main(tk.Frame):
@@ -12,23 +14,45 @@ class Main(tk.Frame):
         self.view_records()
 
     def init_main(self):
-        toolbar = tk.Frame(bg='#d7d8e0', bd=5)
+        toolbar = tk.Frame(bg='#d7d8e0', bd=10)
         toolbar.pack(side=tk.TOP, fill=tk.X)
 
-        self.add_img = tk.PhotoImage(file='img/add.png')
-        btn_open_dialog = tk.Button(master=toolbar, text='Добавить позицию', command=self.open_dialog, bg='#d7d8e0',
-                                    bd=0, compound=tk.TOP, image=self.add_img)
+        # primary actions
+        self.add_img = tk.PhotoImage(file='img/add.gif')
+        btn_open_dialog = tk.Button(master=toolbar, text='Add', command=self.open_dialog, bg='#d7d8e0',
+                                    bd=1, compound=tk.TOP, image=self.add_img, width=55, height=55)
         btn_open_dialog.pack(side=tk.LEFT)
 
-        self.update_img = tk.PhotoImage(file='img/update.png')
-        btn_edit_dialog = tk.Button(master=toolbar, text='Редактировать', bg='#d7d8e0', bd=0, image=self.update_img,
-                                    compound=tk.TOP, command=self.open_update)
-        btn_edit_dialog.pack(side=tk.LEFT)
+        self.search_img = tk.PhotoImage(file='img/search.gif')
+        btn_search = tk.Button(master=toolbar, text='Search', bg='#d7d8e0', bd=1, image=self.search_img,
+                               compound=tk.TOP, command=self.open_search, width=55, height=55)
+        btn_search.pack(side=tk.LEFT)
 
-        self.delete_img = tk.PhotoImage(file='img/delete.png')
-        btn_delete_dialog = tk.Button(master=toolbar, text='Удалить', bg='#d7d8e0', bd=0, image=self.delete_img,
-                                      compound=tk.TOP, command=self.delete_records)
-        btn_delete_dialog.pack(side=tk.LEFT)
+        self.refresh_img = tk.PhotoImage(file='img/refresh.gif')
+        btn_refresh = tk.Button(master=toolbar, text='Refresh', bg='#d7d8e0', bd=1, image=self.refresh_img,
+                                compound=tk.TOP, command=self.view_records, width=55, height=55)
+        btn_refresh.pack(side=tk.LEFT)
+
+        self.delete_all_img = tk.PhotoImage(file='img/delete.gif')
+        btn_delete_all = tk.Button(master=toolbar, text='Clean', bg='#d7d8e0', bd=1, width=55, height=55,
+                                   image=self.delete_all_img, compound=tk.TOP, command='')
+        btn_delete_all.pack(side=tk.LEFT)
+
+        # additional actions
+        self.delete_img = tk.PhotoImage(file='img/delete.gif')
+        btn_delete = tk.Button(master=toolbar, text='Delete', bg='#d7d8e0', bd=1, image=self.delete_img,
+                               compound=tk.TOP, command=self.delete_records, width=55, height=55)
+        btn_delete.pack(side=tk.RIGHT)
+
+        self.update_img = tk.PhotoImage(file='img/update.gif')
+        btn_edit_dialog = tk.Button(master=toolbar, text='Edit', bg='#d7d8e0', bd=1, image=self.update_img,
+                                    compound=tk.TOP, command=self.open_update, width=55, height=55)
+        btn_edit_dialog.pack(side=tk.RIGHT)
+
+        self.calculate_img = tk.PhotoImage(file='img/dollar.gif')
+        btn_delete = tk.Button(master=toolbar, text='Calculate', bg='#d7d8e0', bd=1, image=self.calculate_img,
+                               compound=tk.TOP, command=self.delete_records, width=55, height=55)
+        btn_delete.pack(side=tk.RIGHT)
 
         self.tree = ttk.Treeview(self, columns=('ID', 'description', 'costs', 'price', 'date'), height=15,
                                  show='headings')
@@ -45,11 +69,23 @@ class Main(tk.Frame):
         self.tree.heading('price', text='Сумма')
         self.tree.heading('date', text='дата')
 
-        self.tree.pack()
+        self.tree.pack(side=tk.LEFT)
+
+        scroll = tk.Scrollbar(self, command=self.tree.yview)
+        scroll.pack(side=tk.LEFT, fill=tk.Y)
+        self.tree.configure(yscrollcommand=scroll.set)
 
     def records(self, description, costs, price):
-        self.database.insert_data(description, costs, price)
-        self.view_records()
+        if costs in ('Расход', 'Доход'):
+            self.database.insert_data(description, costs, price)
+            self.view_records()
+        else:
+            messagebox.showwarning("Ошибка заполнения!", f"Нет такого действия - '{costs}'!")
+        try:
+            float(price)
+        except Exception:
+            messagebox.showwarning("Ошибка заполнения!",
+                                   f"Допустимы только числа(простые и десятичные) - '{price}'!\nP.S. Десятичные писать через точку! ")
 
     def update_records(self, description, costs, price):
         self.database.cursor.execute("""UPDATE control SET description=?, costs=?, price=?, date=? WHERE ID=?""",
@@ -59,15 +95,25 @@ class Main(tk.Frame):
         self.view_records()
 
     def delete_records(self):
-        for selection_item in self.tree.selection():
-            self.database.cursor.execute("""DELETE FROM control WHERE id=?""", (self.tree.set(selection_item, '#1'),))
-            self.database.connection.commit()
-            self.view_records()
+        if self.tree.selection():
+            for selection_item in self.tree.selection():
+                self.database.cursor.execute('''DELETE FROM control WHERE id=?''',
+                                             (self.tree.set(selection_item, '#1'),))
+        else:
+            messagebox.showwarning("Ошибка", "Выберите запись(-и) для удаления!")
+        self.database.connection.commit()
+        self.view_records()
 
     def view_records(self):
         self.database.cursor.execute("""SELECT * FROM control""")
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end', values=row) for row in self.database.cursor.fetchall()]
+
+    def search_records(self, description):
+        description = ('%' + description + '%',)
+        self.database.cursor.execute('''SELECT * FROM control WHERE description LIKE ?''', description)
+        [self.tree.delete(i) for i in self.tree.get_children()]
+        [self.tree.insert('', 'end', values=row) for  row in self.database.cursor.fetchall()]
 
     def open_dialog(self):
         Child()
@@ -75,16 +121,18 @@ class Main(tk.Frame):
     def open_update(self):
         Update()
 
+    def open_search(self):
+        Search()
+
 
 class Child(tk.Toplevel):
     def __init__(self):
         super().__init__(window)
-        self.init_child()
         self.view = app
+        self.init_child()
 
-    def add(self):
+    def add_a_mark(self):
         self.view.records(self.entry_description.get(), self.combobox.get(), self.entry_price.get())
-        self.destroy()
 
     def init_child(self):
         self.title('Добавить расходы/доходы')
@@ -104,18 +152,18 @@ class Child(tk.Toplevel):
         self.entry_price = ttk.Entry(self)
         self.entry_price.place(x=200, y=100)
 
-        self.combobox = ttk.Combobox(self, values=[u'Доход', u'Расход'])
+        self.combobox = ttk.Combobox(self, values=['Доход', 'Расход'])
         self.combobox.current(0)
         self.combobox.place(x=200, y=70)
 
-        self.btn_save_and_continue = ttk.Button(self, text='Сохранить и продолжить')
+        self.btn_save_and_continue = ttk.Button(self, text='Сохранить и продолжить', command=self.add_a_mark)
         self.btn_save_and_continue.place(x=57, y=155)
-        self.btn_save_and_continue.bind('<Button-1>', lambda event: self.view.records(self.entry_description.get(),
-                                                                                      self.combobox.get(),
-                                                                                      self.entry_price.get(),
-                                                                                      ))
-        self.btn_add = ttk.Button(self, text='Добавить', command=self.add)
+
+        self.btn_add = ttk.Button(self, text='Добавить')
         self.btn_add.place(x=210, y=155)
+        self.btn_add.bind('<Button-1>', lambda event: self.add_a_mark())
+        self.btn_add.bind('<Button-1>', lambda event: self.destroy(), add='+')
+
         btn_cancel = ttk.Button(self, text='Отмена', command=self.destroy)
         btn_cancel.place(x=290, y=155)
 
@@ -127,6 +175,8 @@ class Update(Child):
     def __init__(self):
         super().__init__()
         self.init_edit()
+        self.database = database
+        self.default_data()
 
     def update(self):
         self.view.update_records(self.entry_description.get(), self.combobox.get(), self.entry_price.get())
@@ -144,29 +194,44 @@ class Update(Child):
                                                                                ))
         self.btn_add.destroy()
 
+    def default_data(self):
+        try:
+            self.database.cursor.execute('''SELECT * FROM control WHERE id=?''',
+                                         (self.view.tree.set(self.view.tree.selection()[0], '#1'),))
+            row = self.database.cursor.fetchone()
+            self.entry_description.insert(0, row[1])
+            if row[2] != 'Доход':
+                self.combobox.current(1)
+            self.entry_price.insert(0, row[3])
+        except IndexError:
+            self.destroy()
+            messagebox.showwarning("Ошибка", "Выберите запись для изменения!")
 
-class DataBase:
+
+class Search(tk.Toplevel):
     def __init__(self):
-        self.connection = sqlite3.connect("control.db")
-        self.cursor = self.connection.cursor()
-        self.cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS control (
-            id integer primary key,
-            description text,
-            costs text,
-            price real,
-            date text)
-            """
-        )
-        self.connection.commit()
+        super().__init__()
+        self.init_search()
+        self.view = app
 
-    def insert_data(self, description, costs, price):
-        self.cursor.execute(
-            """INSERT INTO control (description, costs, price, date) VALUES (?, ?, ?, ?)""",
-            (description, costs, price, str(datetime.now())[:19])
-        )
-        self.connection.commit()
+    def init_search(self):
+        self.title('Поиск')
+        self.geometry('300x100+400+300')
+        self.resizable(False, False)
+
+        lbl_search = tk.Label(self, text='Поиск')
+        lbl_search.place(x=50, y=20)
+
+        self.entry_search = ttk.Entry(self)
+        self.entry_search.place(x=105, y=20, width=150)
+
+        btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
+        btn_cancel.place(x=185, y=50)
+
+        btn_search = ttk.Button(self, text='Поиск')
+        btn_search.place(x=105, y=50)
+        btn_search.bind('<Button-1>', lambda event: self.view.search_records(self.entry_search.get()))
+        btn_search.bind('<Button-1>', lambda event: self.destroy(), add='+')
 
 
 if __name__ == '__main__':
@@ -175,6 +240,7 @@ if __name__ == '__main__':
     app = Main(window)
     app.pack()
     window.title('Cost control')
-    window.geometry('650x450+300+200')
+    window.geometry('665x430+300+200')
     window.resizable(False, False)
+    tk.Label(text='Version 1.1').pack()
     app.mainloop()
