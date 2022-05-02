2e23e2ae61bdf9594bc39f77ab17cb21362d39ae
Lanterman <splen1011@mail.ru>
2022-05-02 17:42:46 +0300

    added popup menu for each entry

--- project/main.py
+++ project/main.py
@@ -5,8 +5,7 @@ from kivymd.uix.menu import MDDropdownMenu
 from project.database import DataBase, CostControl
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDIconButton
-from kivymd.uix.list import IRightBody, ThreeLineAvatarIconListItem
+from kivymd.uix.list import ThreeLineAvatarIconListItem
 
 Window.size = (370, 720)
 
@@ -15,36 +14,33 @@ class MainWindow(MDBoxLayout):
     pass
 
 
-class RightButton(IRightBody, MDIconButton):
-    pass
-
-
 class SearchResultItem(ThreeLineAvatarIconListItem):
     def __init__(self, report_id, db, **kwargs):
         super(SearchResultItem, self).__init__(**kwargs)
         self.db = db
         self.report_id = report_id
-        self.app = MDApp.get_running_app()
-        menu_items = [
-            {
-                "text": f"Item {i}",
-                "viewclass": "OneLineListItem",
-                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
-            } for i in range(5)
-        ]
-        self.menu = MDDropdownMenu(
 
-            items=menu_items,
-            width_mult=4
-        )
+    def show_menu(self):
+        items = [{"text": "Show", "viewclass": "OneLineListItem", "on_release": lambda: self.show_report()},
+                 {"text": "Edit", "viewclass": "OneLineListItem", "on_release": lambda: self.edit_report()},
+                 {"text": "Remove", "viewclass": "OneLineListItem", "on_release": lambda: self.delete_report()}
+                 ]
+        self.menu = MDDropdownMenu(caller=self.ids.button, items=items)
+        self.menu.open()
 
-    def menu_callback(self, text_item):
-        print(text_item)
+    def show_report(self):
+        self.menu.dismiss()
+        print('Done!')
+
+    def edit_report(self):
+        self.menu.dismiss()
+        print(self.ids.button)
 
     def delete_report(self):
         self.db.connection.query(CostControl).filter_by(id=self.report_id).delete()
         self.parent.remove_widget(self)
-        self.db.connection.commit()
+        self.menu.dismiss()
+        # self.db.connection.commit()
 
 
 class CostControlApp(MDApp):
@@ -70,17 +66,20 @@ class CostControlApp(MDApp):
 
     def searching_results(self, query):
         query = f'%{query}%'
-        result = self.db.connection.query(CostControl).filter(CostControl.description.ilike(query))
+        result = self.db.connection.query(CostControl).filter(CostControl.description.ilike(query)).all()
         app = MDApp.get_running_app()
         result_list_widget = app.root.ids.show_result
         result_list_widget.clear_widgets()
-        for report in result:
-            space = 27 - len(report.costs) - len(str(report.price))
-            result_list_widget.add_widget(
-                SearchResultItem(text=f'{report.description}',
-                                 secondary_text=report.costs + space * " " + str(report.price) + ' BYN',
-                                 report_id=report.id, db=db, tertiary_text=f'{report.date}')
-            )
+        if result:
+            for report in result:
+                space = 27 - len(report.costs) - len(str(report.price))
+                result_list_widget.add_widget(
+                    SearchResultItem(text=f'{report.description}',
+                                     secondary_text=report.costs + space * " " + str(report.price) + ' BYN',
+                                     report_id=report.id, db=db, tertiary_text=f'{report.date}')
+                )
+        else:
+            result_list_widget.add_widget(MDLabel(text="Нет записей", halign='center'))
 
     def insert_data(self, description, category, cost, price):
         self.db.insert_data(description, category, cost, price)
@@ -97,4 +96,3 @@ class CostControlApp(MDApp):
 if __name__ == '__main__':
     db = DataBase()
     CostControlApp(db).run()
-1
