d281c36f56f04fd09e8676add5cb6d56344d2b03
Lanterman <splen1011@mail.ru>
2022-05-02 12:57:14 +0300

    main tab design

--- project/main.py
+++ project/main.py
@@ -1,10 +1,14 @@
-from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDIconButton
-from kivymd.uix.list import IRightBody, TwoLineAvatarIconListItem, ThreeLineAvatarIconListItem
+from kivy.core.window import Window
+from kivymd.uix.label import MDLabel
 from kivymd.uix.menu import MDDropdownMenu
 
 from project.database import DataBase, CostControl
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.list import IRightBody, ThreeLineAvatarIconListItem
+
+Window.size = (370, 720)
 
 
 class MainWindow(MDBoxLayout):
@@ -20,6 +24,22 @@ class SearchResultItem(ThreeLineAvatarIconListItem):
         super(SearchResultItem, self).__init__(**kwargs)
         self.db = db
         self.report_id = report_id
+        self.app = MDApp.get_running_app()
+        menu_items = [
+            {
+                "text": f"Item {i}",
+                "viewclass": "OneLineListItem",
+                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
+            } for i in range(5)
+        ]
+        self.menu = MDDropdownMenu(
+
+            items=menu_items,
+            width_mult=4
+        )
+
+    def menu_callback(self, text_item):
+        print(text_item)
 
     def delete_report(self):
         self.db.connection.query(CostControl).filter_by(id=self.report_id).delete()
@@ -35,25 +55,31 @@ class CostControlApp(MDApp):
     def all_reports(self):
         result = self.db.connection.query(CostControl).all()
         app = MDApp.get_running_app()
-        result_list_widget = app.root.ids.all_report
+        result_list_widget = app.root.ids.show_result
         result_list_widget.clear_widgets()
-        for report in result:
-            result_list_widget.add_widget(
-                SearchResultItem(text=f'{report.description}',
-                                 secondary_text=f'{report.category} {report.costs} {report.price}BYN',
-                                 report_id=report.id, db=db, tertiary_text=f'{report.date}')
-            )
+        if result:
+            for report in result:
+                space = 27 - len(report.costs) - len(str(report.price))
+                result_list_widget.add_widget(
+                    SearchResultItem(text=f'{report.description}',
+                                     secondary_text=report.costs + space * " " + str(report.price) + ' BYN',
+                                     report_id=report.id, db=db, tertiary_text=f'{report.date}')
+                )
+        else:
+            result_list_widget.add_widget(MDLabel(text="Нет записей", halign='center'))
 
     def searching_results(self, query):
         query = f'%{query}%'
         result = self.db.connection.query(CostControl).filter(CostControl.description.ilike(query))
         app = MDApp.get_running_app()
-        result_list_widget = app.root.ids.search_result
+        result_list_widget = app.root.ids.show_result
         result_list_widget.clear_widgets()
         for report in result:
+            space = 27 - len(report.costs) - len(str(report.price))
             result_list_widget.add_widget(
-                SearchResultItem(text=f'{report.description} {report.date}', secondary_text='tel',
-                                 report_id=report.id, db=db)
+                SearchResultItem(text=f'{report.description}',
+                                 secondary_text=report.costs + space * " " + str(report.price) + ' BYN',
+                                 report_id=report.id, db=db, tertiary_text=f'{report.date}')
             )
 
     def insert_data(self, description, category, cost, price):
@@ -71,3 +97,4 @@ class CostControlApp(MDApp):
 if __name__ == '__main__':
     db = DataBase()
     CostControlApp(db).run()
+1
