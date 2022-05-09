6cf734314e12f5e85dad44de82c41cac63d5a574
Lanterman <splen1011@mail.ru>
2022-05-09 14:42:32 +0300

    changed edit menu, create apply_change button

--- project/main.py
+++ project/main.py
@@ -1,10 +1,11 @@
+from datetime import datetime
+
 from kivy.core.window import Window
-from kivy.uix.widget import Widget
-from kivymd.uix.button import MDFloatingActionButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.label import MDLabel
 from kivymd.uix.menu import MDDropdownMenu
-from kivymd.uix.textfield import MDTextField
+from kivymd.uix.snackbar import Snackbar
+from sqlalchemy import desc
 
 from project.database import DataBase, CostControl
 from kivymd.app import MDApp
@@ -30,68 +31,51 @@ class DropDownMenuReportsBox(MDDropdownMenu):
     pass
 
 
-class BoxSelectItemEditReport(MDBoxLayout):
-    def __init__(self, name, value, **kwargs):
+class BoxItemEditReport(MDBoxLayout):
+    def __init__(self, db, report_id, **kwargs):
         super().__init__(**kwargs)
-        self.name = name
-        self.value = value
+        self.db = db
+        self.report = self.db.connection.query(CostControl).get(report_id)
         self.default_values()
 
     def default_values(self):
-        self.ids.drop_item_category.set_item(self.value)
-        self.ids.drop_item_costs.set_item("hi")
+        self.ids.changed_description.set_text(instance=None, text=self.report.description)
+        self.ids.drop_item_category.set_item(self.report.category)
+        self.ids.drop_item_costs.set_item(self.report.costs)
+        self.ids.changed_price.set_text(instance=None, text=str(self.report.price))
 
     def drop_down_category_menu(self):
         category = ('---------', 'продукты', 'транспорт', 'связь', 'работа', 'хобби', 'дом', 'копилка')
         menu_items = [{"text": item, "viewclass": "OneLineListItem",
-                       "on_release": lambda x=item: self.set_item_category(x)} for item in category]
+                       "on_release": lambda item=item: self.set_item(item)} for item in category]
         self.menu = DropDownMenuReportsBox(caller=self.ids.drop_item_category, items=menu_items)
         self.menu.open()
 
     def drop_down_costs_menu(self):
         costs = ('Расход', 'Доход')
         menu_items = [{"text": item, "viewclass": "OneLineListItem",
-                       "on_release": lambda x=item: self.set_item_costs(x)} for item in costs]
-        self.menu = DropDownMenuReportsBox(caller=self.ids.drop_item_category, items=menu_items)
+                       "on_release": lambda item=item: self.set_item(item)} for item in costs]
+        self.menu = DropDownMenuReportsBox(caller=self.ids.drop_item_costs, items=menu_items, max_height=98)
         self.menu.open()
 
-    def set_item_category(self, item):
-        self.ids.drop_item_category.set_item(item)
+    def set_item(self, item):
+        if item in ('Расход', 'Доход'):
+            self.ids.drop_item_costs.set_item(item)
+        else:
+            self.ids.drop_item_category.set_item(item)
         self.menu.dismiss()
 
-    def set_item_costs(self, item):
-        self.ids.drop_item_costs.set_item(item)
-        self.menu.dismiss()
-
-
-class BoxItemEditReport(MDBoxLayout):
-    def __init__(self, name, value, **kwargs):
-        super().__init__(**kwargs)
-        self.required = True
-        self.name = name
-        self.value = value
-        self.add_box_item()
-
-    def add_box_item(self):
-        self.add_widget(MDLabel(text=self.name))
-        self.add_widget(MDTextField(text=self.value))
-
-
-class ClassEditReport(MDBoxLayout):
-    def __init__(self, db, report_id, **kwargs):
-        super().__init__(**kwargs)
-        self.db = db
-        self.report_id = report_id
-        self.box_widget()
-
-    def box_widget(self):
-        report = self.db.connection.query(CostControl).filter_by(id=self.report_id)
-        for item in report:
-            self.add_widget(BoxItemEditReport(name='Description', value=item.description))
-            self.add_widget(BoxSelectItemEditReport(name='Category', value=item.category))
-            self.add_widget(BoxItemEditReport(name='Price', value=str(item.price)))
-            self.add_widget(Widget())
-            self.add_widget(MDFloatingActionButton(icon="plus"))
+    def apply_change(self, description, category, cost, price):
+        self.report.description = description
+        self.report.category = category
+        self.report.costs = cost
+        self.report.price = price
+        self.report.date = str(datetime.now())[:19]
+        self.db.connection.add(self.report)
+        self.db.connection.commit()
+        Snackbar(text=25 * " " + f"Record {self.report.id} updated!", font_size=18).open()
+        app = MDApp.get_running_app()
+        app.all_reports()
 
 
 class MainNavigationItem(ThreeLineAvatarIconListItem):
@@ -99,6 +83,7 @@ class MainNavigationItem(ThreeLineAvatarIconListItem):
         super(MainNavigationItem, self).__init__(**kwargs)
         self.db = db
         self.report_id = report_id
+        self.report = self.db.connection.query(CostControl).get(self.report_id)
 
     def show_menu(self):
         items = [{"text": "Show", "viewclass": "OneLineListItem", "on_release": lambda: self.show_report()},
@@ -110,26 +95,24 @@ class MainNavigationItem(ThreeLineAvatarIconListItem):
 
     def show_report(self):  # Применить Item или content_cls
         self.menu.dismiss()
-        report = self.db.connection.query(CostControl).filter_by(id=self.report_id)
-        for item in report:
-            self.dialog = MDDialog(
-                title=12 * " " + 'All information',
-                text=f"Description:       {item.description}\n\n"
-                     f"Category:           {item.category}\n\n"
-                     f"Cost:                   {item.costs}\n\n"
-                     f"Price:                  {item.price}\n\n"
-                     f"Date:                   {item.date}"
-            )
-        self.dialog.open()
+        dialog_show_report = MDDialog(
+            title=12 * " " + 'All information',
+            text=f"Description:       {self.report.description}\n\n"
+                 f"Category:           {self.report.category}\n\n"
+                 f"Cost:                   {self.report.costs}\n\n"
+                 f"Price:                  {self.report.price}\n\n"
+                 f"Date:                   {self.report.date}"
+        )
+        dialog_show_report.open()
 
     def edit_report(self):
         self.menu.dismiss()
-        dialog = MDDialog(
+        dialog_edit_report = MDDialog(
             title=18 * " " + "Edit report",
             type="custom",
-            content_cls=ClassEditReport(self.db, self.report_id)
+            content_cls=BoxItemEditReport(db=self.db, report_id=self.report.id),
         )
-        dialog.open()
+        dialog_edit_report.open()
 
     def delete_report(self):
         self.db.connection.query(CostControl).filter_by(id=self.report_id).delete()
@@ -144,12 +127,13 @@ class CostControlApp(MDApp):
         self.db = db
 
     def all_reports(self):
-        result = self.db.connection.query(CostControl).all()
+        result = self.db.connection.query(CostControl).order_by(desc(CostControl.date)).all()
         self.show_results(result)
 
     def searching_results(self, query):
         query = f'%{query}%'
-        result = self.db.connection.query(CostControl).filter(CostControl.description.ilike(query)).all()
+        result = self.db.connection.query(CostControl).filter(
+            CostControl.description.ilike(query)).order_by(desc(CostControl.date)).all()
         self.show_results(result)
 
     def show_results(self, query):
